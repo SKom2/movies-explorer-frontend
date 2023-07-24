be4e3a6553badcca5d7ee34e16de4f9e93b0cb0e
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-07-24 16:44:47 +0300

    Initial chenges of page

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -1,1 +1,22 @@
+import styles from './AboutProject.module.css';
 
+export default function AboutProject() {
+
+
+    return(
+        <div id="aboutproject" className={styles.aboutProject}>
+            <h2 className={styles.aboutProjectTitle}>О проекте</h2>
+            <div className={styles.line}></div>
+            <div className={styles.grid}>
+                <div className={styles.gridColumn}>
+                    <h3 className={styles.gridTitle}>Дипломный проект включал 5 этапов</h3>
+                    <p className={styles.gridText}>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className={styles.gridColumn}>
+                    <h3 className={styles.gridTitle}>На выполнение диплома ушло 5 недель</h3>
+                    <p className={styles.gridText}>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import styles from './Main.module.css'
 import Promo from "./Promo/Promo";
+import AboutProject from "./AboutProject/AboutProject";
 
 export default function Main() {
     return (
         <section className={styles.main}>
             <Promo />
+            <AboutProject />
         </section>
     )
 }
