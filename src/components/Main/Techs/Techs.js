2a1be27a078ede9cb9bf5de3b10bcf7cce746f06
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-07-26 02:49:26 +0300

    new Techs Component

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -3,8 +3,19 @@ import Heading from "../../Common/Heading/Heading";
 
 export default function Techs(){
     return (
-        <div className={styles.techs}>
+        <div id="technologies" className={styles.techs}>
             <Heading title="Технологии"/>
+            <h3 className={styles.title}>7 технологий</h3>
+            <p className={styles.text}>На курсе веб-разработки мы освоили технологии, которые применили в<br/>дипломном проекте.</p>
+            <div className={styles.grid}>
+                <div className={styles.gridItem}>HTML</div>
+                <div className={styles.gridItem}>CSS</div>
+                <div className={styles.gridItem}>JS</div>
+                <div className={styles.gridItem}>React</div>
+                <div className={styles.gridItem}>Git</div>
+                <div className={styles.gridItem}>Express.js</div>
+                <div className={styles.gridItem}>mongoDB</div>
+            </div>
         </div>
     )
 }
